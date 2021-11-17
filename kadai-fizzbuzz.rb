67ed14169ec589e0003f353e6527ace140691573
y.sasamori <info.yuu09@gmail.com>
2021-11-17 10:31:43 +0000

    fizzbuzz resubmit

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,21 +1,23 @@
 num_max = 100
 
-(1..num_max).each do |num|
-  def fizzbuzz(num)
+def fizzbuzz(num)
     if num % 3 == 0 && num % 5 == 0
-        puts 'FizzBuzz'
-      elsif
-        num % 3 == 0
-        puts 'Fizz'
-      elsif 
-        num % 5 == 0
-        puts 'Buzz'
-      else
-        puts num
-    end
+      puts 'FizzBuzz'
+    elsif
+      num % 3 == 0
+      puts 'Fizz'
+    elsif 
+      num % 5 == 0
+      puts 'Buzz'
+    else
+      puts num
   end
-  puts fizzbuzz(num)
 end
+
+(1..num_max).each do |num|
+  fizzbuzz(num)
+end
+
 
 
 
